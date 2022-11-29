ddf189bfa45aa067e87325c6f1bdbcd18cf9b33e
Noorhani09 <114984404+Noorhani09@users.noreply.github.com>
2022-11-29 21:45:37 +0800

    Add files via upload

---
+++ praktikum5/Paint.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum5.soal1;
+public class Paint {
+    
+    private double coverage;
+    
+    public Paint(double c){
+        coverage = c;
+    }
+    
+    public double amount(Shape s){
+        return s.area() / coverage;
+    }
+}
---
+++ praktikum5/Shape.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum5.soal1;
+public abstract class Shape {
+    protected String shapeName;
+    
+    public Shape(String name){
+        shapeName = name;
+    }
+    
+    public abstract double area();
+    
+    public String toString(){
+        return shapeName;
+    }
+}
